8b93304585d4ef4d229c13e65c66b475a64d8e1b
Fuxuan-CN <3072252442@qq.com>
2025-07-27 18:11:05 +0800

    feat(api/user_controller.py services/user_service.py): Update UserController

--- api/user_controller.py
+++ api/user_controller.py
@@ -1,18 +1,20 @@
 """ 用户控制器 """
 from typing import Literal
 from fastapi import APIRouter
+from fastapi import HTTPException
+from services.user_service import UserService
 
 user_router = APIRouter(prefix="/user", tags=["用户模块"])
 
 @user_router.post("/login")
-async def login(account: str, login_type: Literal['username', 'email', 'phone'], password: str) -> str:
+async def login(login_type: Literal['username', 'email', 'phone'], account: str, password: str) -> str:
     """ 登录 """
     match login_type:
         case "username":
-            return "username login method"
+            return await UserService.login_with_username(account, password)
         case "email":
-            return "email login method"
+            return await UserService.login_with_email(account, password)
         case "phone":
-            return "phone method"
+            return await UserService.login_with_phone(account, password)
         case _:
-            raise Exception("unknown login method")
+            raise HTTPException(400, "unknown login method")
--- services/user_service.py
+++ services/user_service.py
@@ -1,1 +1,20 @@
 
+
+
+class UserService:
+    """ 用户服务 """
+
+    @staticmethod
+    async def login_with_username(username: str, password: str) -> str:
+        """ 用 username 方法登录 """
+        return f"username: {username}, password: {password}"
+
+    @staticmethod
+    async def login_with_phone(phone: str, password: str) -> str:
+        """ 用 phone 方法登录 """
+        return f"phone: {phone}, password: {phone}"
+
+    @staticmethod
+    async def login_with_email(email: str, password: str) -> str:
+        """ 用 email 方法登录 """
+        return f"email: {email}, password: {password}"
